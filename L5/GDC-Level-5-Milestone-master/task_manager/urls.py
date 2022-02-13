0f1a24cfea3fae6645c6ee6eb99096a6a4def157
The-Anton <shekhar59324@gmail.com>
2022-02-13 16:17:28 +0530

    add: l5 url patterns

--- L5/GDC-Level-5-Milestone-master/task_manager/urls.py
+++ L5/GDC-Level-5-Milestone-master/task_manager/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from tasks.views import delete_task_view, task_view, add_task_view, delete_task_view, completed_task_view, done_task_view, all_task_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("tasks/", task_view),
+    path("add-task/", add_task_view),
+    path("delete-task/<int:index>", delete_task_view),
+    path("completed_task/", completed_task_view),
+    path("completed_task/<int:index>/", done_task_view),
+    path("all_tasks/", all_task_view)
 ]
