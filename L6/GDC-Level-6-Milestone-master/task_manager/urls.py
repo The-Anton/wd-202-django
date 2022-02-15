b5b0346eeead9bc6ace25d4215ab65c04914cc58
The-Anton <shekhar59324@gmail.com>
2022-02-15 12:48:44 +0530

    add: l6 milestone url patterns

--- L6/GDC-Level-6-Milestone-master/task_manager/urls.py
+++ L6/GDC-Level-6-Milestone-master/task_manager/urls.py
@@ -1,22 +1,13 @@
-"""task_manager URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-
+from tasks.views import delete_task_view, task_view, add_task_view, delete_task_view, completed_task_view, done_task_view, all_task_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("tasks/", task_view),
+    path("add-task/", add_task_view),
+    path("delete-task/<int:index>", delete_task_view),
+    path("completed_task/", completed_task_view),
+    path("completed_task/<int:index>/", done_task_view),
+    path("all_tasks/", all_task_view)
 ]
