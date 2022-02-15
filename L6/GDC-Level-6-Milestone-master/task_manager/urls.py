e9c768083dfa8896769aef354d5801892f8d9483
The-Anton <shekhar59324@gmail.com>
2022-02-15 18:55:24 +0530

    add: l6 milestone -task update, details, user create and user login

--- L6/GDC-Level-6-Milestone-master/task_manager/urls.py
+++ L6/GDC-Level-6-Milestone-master/task_manager/urls.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 from django.urls import path
-from tasks.views import delete_task_view, task_view, add_task_view, delete_task_view, completed_task_view, done_task_view, all_task_view
+from tasks.views import (GenericCreateTaskView, GenericTaskDeleteView,
+                         GenericTaskView, GenericUpdateTaskView,
+                         UserCreateView, UserLoginView, all_task_view, completed_task_view,
+                         delete_task_view, done_task_view,
+                         sessions_storage_view)
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("tasks/", task_view),
-    path("add-task/", add_task_view),
-    path("delete-task/<int:index>", delete_task_view),
+    path("tasks/", GenericTaskView.as_view()),
+    path("create-task", GenericCreateTaskView.as_view()),
+    path("update-task/<pk>", GenericUpdateTaskView.as_view()),
+    path("delete-task/<pk>", GenericTaskDeleteView.as_view()),
+    path("user/signup", UserCreateView.as_view()),
+    path("user/login", UserLoginView.as_view()),
+    path("user/logout", LogoutView.as_view()),
     path("completed_task/", completed_task_view),
     path("completed_task/<int:index>/", done_task_view),
-    path("all_tasks/", all_task_view)
+    path("all_tasks/", all_task_view),
+    path("sessiontest/", sessions_storage_view)
+
+
 ]
--- L6/GDC-Level-6-Milestone-master/tasks/views.py
+++ L6/GDC-Level-6-Milestone-master/tasks/views.py
@@ -1,10 +1,91 @@
+from re import template
+from django.forms import ModelForm, ValidationError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 
 from tasks.models import Task
 
-completed_tasks = []
-tasks = []
+class UserLoginView(LoginView):
+    template_name = "user_login.html"
+class UserCreateView(CreateView):
+    form_class = UserCreationForm
+    template_name = "user_create.html"
+    success_url = "/user/login"
+
+def sessions_storage_view(request):
+
+    total_views = request.session.get("total_views",0)
+    request.session['total_views'] = total_views + 1
+    return HttpResponse(f"Total Views is {total_views}")
+class TaskCreateFrom(ModelForm):
+
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+        if(len(title) < 10):
+            raise ValidationError("Data too small")
+        return title.upper()
+
+    class Meta:
+        model = Task
+        fields = ["title", "description", "completed"]
+
+class GenericTaskDeleteView(DeleteView):
+    model = Task
+    template_name = "task_delete.html"
+    success_url = "/tasks"
+class GenericDetailTaskView(DetailView):
+    model = Task
+    template_name = "task_detail.html"
+class GenericUpdateTaskView(UpdateView):
+    model = Task
+    form_class = TaskCreateFrom
+    template_name = "task_update.html"
+    success_url = "/tasks"
+class GenericCreateTaskView(CreateView):
+    model = TaskCreateFrom
+    fields = ("title", "description", "completed")
+    template_name = "task_create.html"
+    success_url = "/tasks"
+class GenericTaskView(ListView):
+    model = Task.objects.filter(deleted=False)
+    template_name = "tasks.html"
+    context_object_name = "tasks"
+    paginate_by = 5
+
+    def get_queryset(self):
+        search_term = self.request.GET.get("search")
+        tasks = Task.objects.filter(deleted=False)
+
+        if search_term:
+            tasks = tasks.filter(title__icontains=search_term)
+        return tasks
+class CreateTaskView(View):
+
+    def get(self, request):
+        return render(request, "task_create.html")
+
+    def post(self, request):
+        task_value = request.POST.get("task")
+        Task(title=task_value).save()
+        return HttpResponseRedirect("/tasks")
+class TaskView(View):
+
+    def get(self, request):
+        tasks = Task.objects.filter(deleted=False)
+        search_term = request.GET.get("search")
+
+        if search_term:
+            tasks = tasks.filter(title__icontains=search_term)
+        return render(request, "tasks.html", {"tasks": tasks})
+
+    def post(self, request):
+        pass
 
 def task_view(request):
     tasks = Task.objects.filter(deleted=False)
@@ -12,13 +93,7 @@ def task_view(request):
 
     if search_term:
         tasks = tasks.filter(title__icontains=search_term)
-    return render(request, "tasks.html", {"tasks": tasks})
-
-def add_task_view(request):
-    task_value = request.GET.get("task")
-    Task(title=task_value).save()
-    tasks.append(task_value)
-    return HttpResponseRedirect("/tasks")
+    return render(request, "tasks.html", {"tasks": tasks})  
 
 def delete_task_view(request, index):
     Task.objects.filter(id=index).update(deleted=True)
