d359910b40bc667054eb14d6e6c9a0064625ca74
The-Anton <shekhar59324@gmail.com>
2022-02-16 14:27:51 +0530

    add: nested serializaition

--- L7/GDC-Level-7-Milestone-master/tasks/apiviews.py
+++ L7/GDC-Level-7-Milestone-master/tasks/apiviews.py
@@ -7,15 +7,29 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username")
 class TaskSerializer(ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Task
-        fields = ["title", "description", "completed"]
+        fields = ["title", "description", "completed", "user"]
 
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
-    serializer_class = TaskSerializer
+    serializer_class = TaskSerializer 
+
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user, deleted=False)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class TaskListAPI(View):
 
