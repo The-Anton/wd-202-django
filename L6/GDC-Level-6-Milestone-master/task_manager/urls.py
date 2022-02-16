4a6157301dcdcece36ff8031d91f32e3c3fd8843
The-Anton <shekhar59324@gmail.com>
2022-02-16 11:03:03 +0530

    add: l6 url routes for deltail-task and sessiontest

--- L6/GDC-Level-6-Milestone-master/task_manager/urls.py
+++ L6/GDC-Level-6-Milestone-master/task_manager/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
-from django.urls import path
-from tasks.views import (GenericCreateTaskView, GenericTaskDeleteView,
-                         GenericTaskView, GenericUpdateTaskView,
-                         UserCreateView, UserLoginView, all_task_view, completed_task_view,
-                         delete_task_view, done_task_view,
-                         sessions_storage_view)
 from django.contrib.auth.views import LogoutView
+from django.urls import path
+from tasks.views import (GenericTaskCreateView, GenericTaskDeleteView,
+                         GenericTaskDetailView, GenericTaskUpdateView,
+                         GenericTaskView, UserCreateView, UserLoginView,
+                         all_task_view, completed_task_view, done_task_view,
+                         sessions_storage_view)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tasks/", GenericTaskView.as_view()),
-    path("create-task", GenericCreateTaskView.as_view()),
-    path("update-task/<pk>", GenericUpdateTaskView.as_view()),
+    path("create-task/", GenericTaskCreateView.as_view()),
+    path("update-task/<pk>", GenericTaskUpdateView.as_view()),
+    path("detail-task/<pk>", GenericTaskDetailView.as_view()),
     path("delete-task/<pk>", GenericTaskDeleteView.as_view()),
     path("user/signup", UserCreateView.as_view()),
     path("user/login", UserLoginView.as_view()),
@@ -19,7 +20,7 @@ urlpatterns = [
     path("completed_task/", completed_task_view),
     path("completed_task/<int:index>/", done_task_view),
     path("all_tasks/", all_task_view),
-    path("sessiontest/", sessions_storage_view)
+    path("sessiontest/", sessions_storage_view),
 
 
 ]
