956096d56f952d263e6b7ef1cd4f2a38207798ec
The-Anton <shekhar59324@gmail.com>
2022-02-16 14:47:28 +0530

    add: django-filter with tsk status attribute addition

--- L7/GDC-Level-7-Milestone-master/tasks/apiviews.py
+++ L7/GDC-Level-7-Milestone-master/tasks/apiviews.py
@@ -1,14 +1,21 @@
 from asyncio import tasks
-from django.views import View
-from django.http.response import JsonResponse
 
-from tasks.models import Task
-from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from django.http.response import JsonResponse
+from django.views import View
+from django_filters.rest_framework import (CharFilter, ChoiceFilter,
+                                           DjangoFilterBackend, FilterSet)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.models import User
+
+from tasks.models import Task
+
+
+class TaskFilter(FilterSet):
+    title = CharFilter(lookup_expr="icontains")
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
@@ -24,6 +31,9 @@ class TaskViewSet(ModelViewSet):
     serializer_class = TaskSerializer 
 
     permission_classes = (IsAuthenticated,)
+
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TaskFilter
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user, deleted=False)
--- L7/GDC-Level-7-Milestone-master/tasks/models.py
+++ L7/GDC-Level-7-Milestone-master/tasks/models.py
@@ -3,12 +3,19 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+STATUS_CHOICES = (
+    ("PENDING", "PENDING"),
+    ("IN_PROGRESS", "IN_PROGRESS"),
+    ("COMPLETED", "COMPLETED"),
+    ("CANCELLED", "CANCELLED"),
+)
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     completed = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
     user = models.ForeignKey(User , on_delete=models.CASCADE , null=True,blank=True)
 
     def __str__(self):
